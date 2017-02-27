e77d55064db46320849fe17f103b08db3ea90fcb
Christian Walde <walde.christian@googlemail.com>
2017-02-27 17:55:08 +0100

    tidy

--- menutest.pl
+++ menutest.pl
@@ -3,22 +3,31 @@ use OpenGL::Modern qw(:all);
 
 glutInit();
 
-glutInitWindowPosition(10, 10);
-glutInitWindowSize(200, 200);
-glutInitDisplayMode(16);
+glutInitWindowPosition( 10, 10 );
+glutInitWindowSize( 200, 200 );
+glutInitDisplayMode( 16 );
 
-$win = glutCreateWindow("test2");
-glutSetWindow($win);
+$win = glutCreateWindow( "test2" );
+glutSetWindow( $win );
 
-glutDisplayFunc(sub { print "Display!\n";
-		      glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) } );
-glutReshapeFunc(sub { print "Reshape!\n";
-		      my ($width, $height) = @_;
-		      glViewport(0, 0, $width, $height); } );
+glutDisplayFunc(
+    sub {
+        print "Display!\n";
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    }
+);
+glutReshapeFunc(
+    sub {
+        print "Reshape!\n";
+        my ( $width, $height ) = @_;
+        glViewport( 0, 0, $width, $height );
+    }
+);
+
 #glutIdleFunc(sub { print "Idle!\n" } );
 
-glutCreateMenu(sub { print "Got menu ",@_,"\n" } );
-glutAddMenuEntry("Hello", 1);
-glutAttachMenu(GLUT_RIGHT_BUTTON);
+glutCreateMenu( sub { print "Got menu ", @_, "\n" } );
+glutAddMenuEntry( "Hello", 1 );
+glutAttachMenu( GLUT_RIGHT_BUTTON );
 
 glutMainLoop();
